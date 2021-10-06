630802513625913ce1158ae31769ca6bf5ec0443
Benfild <fildben@gmail.com>
2021-10-06 12:00:03 +0300

    feat: convert img to greyscale and detect edges

--- pnd.py
+++ pnd.py
@@ -6,3 +6,8 @@ from scipy import ndimage
 img = cv2.imread('carsImages/WhatsApp Image 2020-10-15 at 11.23.42.jpeg')
 imgGs = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 imgGS = cv2.GaussianBlur(imgGs, (5, 5), 0)
+
+# Edges detection
+imgEdged = cv2.Canny(imgGs, 63, 200)
+cv2.imshow("Edged Image", imgEdged)
+cv2.waitKey(0)
