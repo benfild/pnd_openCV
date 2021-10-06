0a2b996bafdb603136ab15452b1991ce8f27c7b7
Benfild <fildben@gmail.com>
2021-10-06 11:58:55 +0300

    feat: import dependencies

---
+++ pnd.py
@@ -0,0 +1,8 @@
+import cv2
+import pytesseract
+from scipy import ndimage
+
+# read the image file and convert in grayscale Image
+img = cv2.imread('carsImages/WhatsApp Image 2020-10-15 at 11.23.42.jpeg')
+imgGs = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgGS = cv2.GaussianBlur(imgGs, (5, 5), 0)
